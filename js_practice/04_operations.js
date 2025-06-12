a59d0e1460d1550f0a020003a276bfe30aad1073
yashRay-git <yashsureshraypure.eidiko@gmail.com>
2025-06-12 12:26:31 +0000

    maths operation

---
+++ js_practice/04_operations.js
@@ -0,0 +1,48 @@
+/*console.log(1+2);
+console.log(1-2);
+console.log(1*2);
+console.log(3**2);
+console.log(1/2);
+console.log(1%2);
+*/
+ 
+let str1 ="string";
+let str2 = "variables";
+
+console.log(str1 + str2);
+
+console.log("1"+2);
+console.log("1"+2 +2 ); // 
+console.log(2+2 +"1"); // dont depends on thies 
+
+console.log(true);
+console.log(+ true);
+
+
+//------------postfix-----
+
+let varible;
+let count =100;
+varible= ++count; // here 1st incrementd and then assigned 
+console.log(varible,count); 
+
+//--------------- pots fix example
+
+let cvari;
+let coun1 =100 ;
+ cvari= coun1++; // here 1st 100 is asigned nd then incremented 
+console.log(cvari,coun1);
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
