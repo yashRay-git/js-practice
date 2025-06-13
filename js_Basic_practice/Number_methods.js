758dd612c6e3f0da373cac384530af5539991b9c
yashRay-git <yashsureshraypure.eidiko@gmail.com>
2025-06-13 12:32:48 +0000

    formula

--- js_Basic_practice/Number_methods.js
+++ js_Basic_practice/Number_methods.js
@@ -12,11 +12,15 @@ console.log(myNum.toLocaleString('en-in'));
 console.log('------------------maths function------------------');
 
 console.log(Math.abs(-6));
-console.log(Math.max(-6,4,6,7));
-console.log(Math.min(-6,4,6,7));
-console.log(Math.round(6.7));
-console.log(Math.random()*10 + 1);
+console.log(Math.max(-6,4,6,7)); // gives min values 
+console.log(Math.min(-6,4,6,7)); //give max value 
+console.log(Math.round(6.7)); // round up the figured
+console.log(Math.random()*10 + 1); //generate random number 
 console.log(Math.round((Math.random()*10)) + 1);
 
 
+// if we want specificly two digit number in between  min and max then we can d o it by this  
+const min = 10
+const max = 20
 
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
